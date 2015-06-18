9295fe6572af71916adccfbe9807dcea1e3819f0
jmiddle <jarradm.iic@gmail.com>
2015-06-18 17:31:56 +0900

    Added template pages

--- js/app.js
+++ js/app.js
@@ -4,17 +4,18 @@
 			this.products = lightAndSound;
 		});
 
-	app.controller('TabController',function() {
-    	this.tab = 2;
+	app.controller('TabController', function() {
+    	this.tab = 1;
     	this.setTab = function(selectedTab) {
       		this.tab = selectedTab;
     	};
    	 	this.isSet = function(givenTab) {
      		return this.tab === givenTab;
-    };	
+    	};	
     
-  });
+  	});
 
+	//broken at the moment
 	app.controller('GalleryController', function() {
     	this.current = 0;
     	this.setCurrent = function(assignCurrent) {
@@ -27,12 +28,81 @@
     	};
   	});
 
-	
+  	app.controller('ShoppingBasketController', function() {
+  		this.product = {};
+  		this.basket = [];
+  		this.cost = 0;
+  		this.addToBasket = function(product, amount) {
+  			var basketItem = [product, amount];
+  				
+  			this.cost += product.price * amount;
+  			this.basket.push(basketItem);
+  			
+  		};
 
+  		//need to implement this later and remove cost calculations in addToBasket
+  		this.getTotalCost = function() {
+  			
+  			for (i = 0; i < basket.length(); i++) {
+  				cost += basket[i][0].price * basket[i][1]; 
+  				console.log(cost);
+
+  			}
+  			return cost;
+
+  		};
+  	});
+
+	app.directive("lightsTab", function() {
+    	return {
+      		restrict: 'E',
+      		templateUrl: 'lights.html'
+    	};
+    });
+	
+	app.directive("soundTab", function() {
+    	return {
+      		restrict: 'E',
+      		templateUrl: 'sound.html'
+    	};
+    });
+
+    app.directive("packageTab", function() {
+    	return {
+      		restrict: 'E',
+      		templateUrl: 'package.html'
+    	};
+    });
+
+    app.directive("aboutTab", function() {
+    	return {
+      		restrict: 'E',
+      		templateUrl: 'about.html'
+    	};
+    });
+
+    app.directive("contactTab", function() {
+    	return {
+      		restrict: 'E',
+      		templateUrl: 'contact.html'
+    	};
+    });
+
+    app.directive("shoppingCartTab", function() {
+    	return {
+      		restrict: 'E',
+      		templateUrl: 'shopping-cart.html'
+    	};
+    });
+
+
+
+    // needs to be moved to a JSON file at some stage
 	var lightAndSound = [
 	{ 
 		name: 'Tri-phase', 
 		price: 50.00,
+		description: "		",
 		canPurchase: true,
    		soldOut: false,
    		category: 'light', //options are light, sound or package
@@ -48,26 +118,86 @@
 	{ 
 		name: 'Invader', 
 		price: 65.00,
+		description: "",
 		canPurchase: true,
    		soldOut: false,
-   		category: 'light' 
+   		category: 'light',
+   		images: [
+			"images/image6.jpg"
+		]
    	},
 
    	{ 
 		name: 'Strobe', 
 		price: 20.00,
+		description: "",
 		canPurchase: true,
    		soldOut: false,
-   		category: 'light' 
+   		category: 'light',
+		images: [
+			"images/image7.jpg"
+		]
+
    	},
 
    	{ 
 		name: 'Traffic Light', 
 		price: 50.00,
+		description: "",
 		canPurchase: true,
    		soldOut: false,
-   		category: 'sound' 
-   	}
+   		category: 'light',
+   		images: [
+			"images/image8.jpg"
+		]
+
+   	},
+
+   	{ 
+		name: 'Deluxe Speaker & Lighting Set', 
+		price: 230.00,
+		description: "		",
+		canPurchase: true,
+   		soldOut: false,
+   		category: 'package', 
+		images: [
+			"images/image9.jpg"			
+		]    		
+   	},
+
+   	{ 
+		name: 'Twin Speaker Set 1000RMS', 
+		price: 170.00,
+		description: "		",
+		canPurchase: true,
+   		soldOut: false,
+   		category: 'sound', 
+		images: [
+			"images/image10.jpg"
+		]    		
+
+   	},
+
+   	{ 
+		name: 'Speaker Box', 
+		price: 85,
+		description: "		",
+		canPurchase: true,
+   		soldOut: false,
+   		category: 'sound', 
+		images: [
+			"images/image11.jpg"			
+		]    		
+
+   	},
+
+
+
+
+
+
+
+
    	];
 
 
